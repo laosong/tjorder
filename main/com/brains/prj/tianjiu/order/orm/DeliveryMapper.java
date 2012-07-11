cacf7881906528ece4c443f779e3c12f8ab72e17
laosong <songcw55@gmail.com>
2012-07-11 10:46:32 +0800

    添加送货方式mapper

---
+++ main/com/brains/prj/tianjiu/order/orm/DeliveryMapper.java
@@ -0,0 +1,19 @@
+package com.brains.prj.tianjiu.order.orm;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: songchunwen
+ * Date: 12-7-11
+ * Time: 上午10:40
+ * To change this template use File | Settings | File Templates.
+ */
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.brains.prj.tianjiu.order.domain.*;
+
+public interface DeliveryMapper {
+    List<DeliveryInfo> getDeliveryByState(@Param("state") short state);
+}
